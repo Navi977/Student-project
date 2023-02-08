1317d2f7cd682fe099f89512fc3c9fcac6163555
Navitha G A <124753219+Navi977@users.noreply.github.com>
2023-02-08 11:33:42 +0530

    Update SortStudentByMarks.java

--- SortStudentByMarks.java
+++ SortStudentByMarks.java
@@ -1,1 +1,10 @@
-
+package com;
+import java.util.Comparator;
+public class SortStudentByMarks implements Comparator<Student>
+{
+	@Override
+	public int compare(Student x, Student y)
+	{
+		return x.getMarks() - y.getMarks();
+	}
+}
